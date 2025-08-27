b339875a95731755cdb9c7e72c55589fb7bb0483
Daniel Kim <dqnkjm@gmail.com>
2025-08-27 13:55:06 -0400

    files

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { useState } from 'react';
 import { ToolBarContext } from "./context/ToolBarContext";
 import Nav from "./ui/Nav";
 
+//files
 export default function RootLayout({
   children,
 }: Readonly<{
